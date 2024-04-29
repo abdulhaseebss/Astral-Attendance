30d00b0a9ad32b0574b37c08065e87f5b97b5e39
abdulhaseebss <m.saleemhaseeb@gmail.com>
2024-04-29 16:12:00 +0500

    database connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+const express = require("express");
+const cors = require("cors");
+const db = require('./db.connect.js')
+const app = express();
+const port = 3000;
+
+
+app.use(cors())
+
+
+
+
+
+app.get('/' , (req , res)=>{
+    const sql = "Select * from users"
+    db.query(sql , (err , data)=>{
+        if (err) return res.json(err);
+        return res.json(data)
+    })
+})
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
